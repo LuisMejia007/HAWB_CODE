14f0f5b89c343182c7537469ad56457e61786c9f
Jrod9599 <Jrod9599@gmail.com>
2017-02-04 13:33:51 -0800

    Add files via upload
    Saves data from UV reader and saves to .csv

---
+++ UVreader.ino
@@ -0,0 +1,57 @@
+#include <SD.h>
+#include <Time.h>
+
+#define UVPIN 10
+
+File myFile;
+
+void setup() {
+
+  /*
+   * 
+   * 
+   * 
+   * 
+   */
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(UVPIN, INPUT); 
+  //avoid crash
+  if(!SD.begin(UVPIN)){
+    Serial.println("SD initialization failed!");
+    return;
+  }
+  /*
+   * 
+   * 
+   * 
+   */ 
+   myFile = SD.open("UVindex.csv", FILE_WRITE);
+
+   if(myFile)
+      myFile.write(" \"UVindex\" \n ");
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int UVreading_input = analogRead(UVPIN);
+  int UVindex = (UVreading_input * (5.0 / 102.4));
+
+  myFile = SD.open("UVindex.csv", FILE_WRITE);
+  if(myFile){
+    
+    myFile.write(UVindex);
+    myFile.write("\n");
+    
+  }
+  else{
+    Serial.println("ERROR could not open file");
+  }
+
+  myFile.close();
+  
+  //delay(1000); //1000 miliseconds = 1 second
+  
+}
